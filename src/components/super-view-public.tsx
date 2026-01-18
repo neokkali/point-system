a1d2d556dd0460652922f734ebb5a1319bc7307f
neokkail <qo9@hotmail.fr>
2026-01-18 19:38:24 +0300

    editing owner spliling

--- src/components/super-view-public.tsx
+++ src/components/super-view-public.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useAuthGuard } from "@/hooks/use-auth-guard";
 import SuperScores from "./super-scores";
 
 const SuperViewPublic = () => {
-  useAuthGuard(["OWNER", "ADMIN", "MODERATOR"], "/auth", "/");
   return <SuperScores url="/super/public" />;
 };
 
