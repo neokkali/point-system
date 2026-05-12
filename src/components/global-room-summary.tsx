9984a1c2419b63503e7396dc6b6f9ca202e03872
neokkali <qo9@hotmail.fr>
2026-05-12 05:32:49 +0300

    add new global room sumary for the points

--- src/components/global-room-summary.tsx
+++ src/components/global-room-summary.tsx
@@ -29,7 +29,7 @@ export const GlobalRoomSummary = () => {
   if (error || !data) return null;
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
       {data.map((room: GlobalRoomSummaryTypes) => (
         <Card
           key={room.roomId}
